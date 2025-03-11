a9c55d479ccbb4101632c0b5430316097dade9d9
Julian Lopez <64julianlopez@gmail.com>
2025-03-10 22:28:27 -0700

    Add custom keycodes for Warp and Visual Studio Code shortcuts

--- ansi_encoder/keymaps/default/keymap.c
+++ ansi_encoder/keymaps/default/keymap.c
@@ -33,6 +33,8 @@ enum custom_keycodes {
     MACCY,
     MACOS_SCREENLOCK,
     ID_TECH,
+    OPEN_WARP,
+    OPEN_VS_CODE,
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -79,14 +81,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_LBRC);
         }
         break;
-        case ID_TECH:
+    case ID_TECH:
         if (record->event.pressed) {
-            register_code(KC_LCTL);
-            register_code(KC_LOPT);
-            register_code(KC_1);
-            unregister_code(KC_LCTL);
-            unregister_code(KC_LOPT);
-            unregister_code(KC_1);
+            SEND_STRING(SS_TAP(X_LCMD) SS_TAP(X_LCMD) SS_DELAY(300) "Run iD Tech Shortcut" SS_TAP(X_ENT) );
+        }
+        break;
+        case OPEN_WARP:
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_LCMD) SS_TAP(X_LCMD) SS_DELAY(300) "Open Warp" SS_TAP(X_ENT) SS_DELAY(500) SS_TAP(X_ESC));
+        }
+        break;
+    case OPEN_VS_CODE:
+       if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_LCMD) SS_TAP(X_LCMD) SS_DELAY(300) "Open Visual Studio Code" SS_TAP(X_ENT) SS_DELAY(500) SS_TAP(X_ESC));
         }
         break;
     }
@@ -114,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_MPLY,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     ID_TECH,  KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     OPEN_WARP,KC_Y,     KC_U,     ID_TECH,  KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     RM_TOGG,  KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     MACCY,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        KC_LSFT,            KC_Z,     KC_X,     MACCY,OPEN_VS_CODE, KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_82(
