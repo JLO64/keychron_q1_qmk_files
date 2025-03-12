73a50f07463194420c5256f787cb7a5f7b1f3ea1
Julian Lopez <64julianlopez@gmail.com>
2025-03-12 15:31:48 -0700

    Update keymap and RGB matrix configuration for better functionality
    - Added QK_BOOT to the default keymap.
    - Updated RGB effects to include specific LED indices that were missed in previous configurations.

--- ansi_encoder/keymaps/default/keymap.c
+++ ansi_encoder/keymaps/default/keymap.c
@@ -166,9 +166,9 @@
          KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_MPLY,
          KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
          KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     OPEN_WARP,KC_Y,     KC_U,     ID_TECH,  KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     RM_TOGG,  KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-         KC_LSFT,            KC_Z,     KC_X,     MACCY,OPEN_VS_CODE, KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  FULL_SCREEN_WINDOW,
-         KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  TILE_LEFT,  MAXIMIZE_WINDOW,  TILE_RIGHT),
+         KC_CAPS,  KC_A,     KC_S,     KC_D,     QK_BOOT,  KC_G,     KC_H,     KC_J,     KC_K,     RM_TOGG,  KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+         KC_LSFT,            KC_Z,     KC_X,     MACCY,OPEN_VS_CODE, KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  MAXIMIZE_WINDOW,
+         KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  TILE_LEFT,  FULL_SCREEN_WINDOW,  TILE_RIGHT),
  
      [WIN_FN] = LAYOUT_ansi_82(
          _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            RM_TOGG,
